50dfdd298b380e0264277517e55eab2dbb468e4e
Rodrigue Koffi <bonclay7@users.noreply.github.com>
2022-07-26 20:50:59 +0200

    v1

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "eks_cluster_id" {
+  description = "EKS Cluster Id"
+  value       = var.eks_cluster_id
+}
+
+output "aws_region" {
+  description = "EKS Cluster Id"
+  value       = var.aws_region
+}

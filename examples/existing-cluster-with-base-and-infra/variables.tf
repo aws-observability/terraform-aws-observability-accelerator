6e3ed9be21a7de0b896174616e1aec96845dd515
Rodrigue Koffi <bonclay7@users.noreply.github.com>
2022-08-10 21:50:24 +0200

    Cosmetics

--- examples/existing-cluster-with-base-and-infra/variables.tf
+++ examples/existing-cluster-with-base-and-infra/variables.tf
@@ -1,15 +1,7 @@
-variable "java" {
-  default = {
-    a = ""
-    b = ""
-  }
-}
-
 variable "eks_cluster_id" {
   description = "EKS Cluster Id"
   type        = string
 }
-
 variable "aws_region" {
   description = "AWS Region"
   type        = string
@@ -26,19 +18,15 @@ variable "managed_prometheus_region" {
   type    = string
   default = ""
 }
-
-
 variable "managed_grafana_workspace_id" {
   type    = string
   default = ""
 }
-
 variable "grafana_endpoint" {
   description = "Grafana endpoint"
   type        = string
   default     = null
 }
-
 variable "grafana_api_key" {
   description = "API key for authorizing the Grafana provider to make changes to Amazon Managed Grafana"
   type        = string

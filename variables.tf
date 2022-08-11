74c302e6163126f43b20928429ca0bc9ded69171
Rodrigue Koffi <bonclay7@users.noreply.github.com>
2022-08-11 15:10:49 +0200

    Tidy variables

--- variables.tf
+++ variables.tf
@@ -31,11 +31,6 @@ variable "enable_cert_manager" {
   default     = true
 }
 
-variable "enable_opentelemetry_operator" {
-  type    = bool
-  default = false
-}
-
 variable "enable_managed_prometheus" {
   type    = bool
   default = true
@@ -64,7 +59,6 @@ variable "enable_managed_grafana" {
   default = true
 }
 
-
 variable "managed_grafana_region" {
   description = "AWS Managed Grafana Workspace Region"
   type        = string
@@ -79,64 +73,8 @@ variable "grafana_api_key" {
   default = null
 }
 
-variable "enable_java" {
-  description = "Deploys a collector for JAVA/JMX based workloads, dashboards and alerting rules"
-  type        = bool
-  default     = false
-}
-
-variable "enable_java_recording_rules" {
-  description = "Enable AMP recording rules for Java"
-  type        = bool
-  default     = true
-}
-
-# variable "java_config" {
-#   description = "Input configuration Java workloads"
-#   type = object({
-#     enable_recording_rules = bool
-#   })
-# }
-
-
 variable "tags" {
   description = "Additional tags (e.g. `map('BusinessUnit`,`XYZ`)"
   type        = map(string)
   default     = {}
-}
-
-
-variable "enable_infra_metrics" {
-  type    = bool
-  default = false
-}
-
-variable "infra_metrics_config" {
-  type = object({
-    helm_config = map(any)
-
-    enable_kube_state_metrics = bool
-    kms_create_namespace      = bool
-    ksm_k8s_namespace         = string
-    ksm_helm_chart_name       = string
-    ksm_helm_chart_version    = string
-    ksm_helm_release_name     = string
-    ksm_helm_repo_url         = string
-    ksm_helm_settings         = map(string)
-    ksm_helm_values           = map(any)
-
-    enable_node_exporter  = bool
-    ne_create_namespace   = bool
-    ne_k8s_namespace      = string
-    ne_helm_chart_name    = string
-    ne_helm_chart_version = string
-    ne_helm_release_name  = string
-    ne_helm_repo_url      = string
-    ne_helm_settings      = map(string)
-    ne_helm_values        = map(any)
-
-    enable_dashboards = bool
-  })
-
-  default = null
 }

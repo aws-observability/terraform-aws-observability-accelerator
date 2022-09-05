601adea38b1d1ccaf59a248cc6b79269ba4da3b5
Rodrigue Koffi <bonclay7@users.noreply.github.com>
2022-09-05 17:21:55 +0200

    Remove duplicate, add tagging

--- modules/workloads/infra/dashboards.tf
+++ modules/workloads/infra/dashboards.tf
@@ -16,34 +16,10 @@ resource "grafana_dashboard" "pods" {
   config_json = file("${path.module}/dashboards/pods.json")
 }
 
-resource "grafana_dashboard" "pv" {
-  count       = var.enable_dashboards ? 1 : 0
-  folder      = var.dashboards_folder_id
-  config_json = file("${path.module}/dashboards/pesistentvolumes.json")
-}
-
 resource "grafana_dashboard" "nodes" {
   count       = var.enable_dashboards ? 1 : 0
   folder      = var.dashboards_folder_id
   config_json = file("${path.module}/dashboards/nodes.json")
-}
-
-resource "grafana_dashboard" "necluster" {
-  count       = var.enable_dashboards ? 1 : 0
-  folder      = var.dashboards_folder_id
-  config_json = file("${path.module}/dashboards/nodeexpoter-use-cluster.json")
-}
-
-resource "grafana_dashboard" "nenodeuse" {
-  count       = var.enable_dashboards ? 1 : 0
-  folder      = var.dashboards_folder_id
-  config_json = file("${path.module}/dashboards/nodeexporter-use-node.json")
-}
-
-resource "grafana_dashboard" "nenode" {
-  count       = var.enable_dashboards ? 1 : 0
-  folder      = var.dashboards_folder_id
-  config_json = file("${path.module}/dashboards/nodeexporter-nodes.json")
 }
 
 resource "grafana_dashboard" "nwworload" {
@@ -80,12 +56,6 @@ resource "grafana_dashboard" "kubelet" {
   count       = var.enable_dashboards ? 1 : 0
   folder      = var.dashboards_folder_id
   config_json = file("${path.module}/dashboards/kubelet.json")
-}
-
-resource "grafana_dashboard" "controller" {
-  count       = var.enable_dashboards ? 1 : 0
-  folder      = var.dashboards_folder_id
-  config_json = file("${path.module}/dashboards/controller.json")
 }
 
 resource "grafana_dashboard" "clusternw" {

c9f783db11f4e047468cc89de3f600f7af1b1ccb
Rodrigue Koffi <bonclay7@users.noreply.github.com>
2022-09-23 16:18:52 +0200

    Setup actions for pre-commit (#34)
    * Setup actions for pre-commit
    
    * Link check
    
    * To drop
    
    * Edit path
    
    * Pre-commit badge
    
    * Revert "Edit path"
    
    This reverts commit ccd5c09bf8da61489ec8c2bfa35f6ac8c0a61005.
    
    * Revert "To drop"
    
    This reverts commit a3ccee05faabff7ba28b600acc83cea3f018414f.
    
    * Setup plan for examples
    
    * Remove dead code

--- examples/existing-cluster-with-base-and-infra/versions.tf
+++ examples/existing-cluster-with-base-and-infra/versions.tf
@@ -22,4 +22,12 @@ terraform {
       version = ">= 1.25.0"
     }
   }
+
+  # ##  Used for end-to-end testing on project; update to suit your needs
+  # backend "s3" {
+  #   bucket = "observability-accelerator-terraform-states"
+  #   region = "us-west-2"
+  #   key    = "e2e/existing-cluster-with-base-and-infra/terraform.tfstate"
+  # }
+
 }

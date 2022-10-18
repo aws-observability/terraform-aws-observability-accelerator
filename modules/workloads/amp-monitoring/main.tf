028be52cae729f52c931f1e272e565730d6ef203
Kevin Lewin <lewinke@amazon.com>
2022-10-18 09:44:22 -0400

    adding CW datasource

--- modules/workloads/amp-monitoring/main.tf
+++ modules/workloads/amp-monitoring/main.tf
@@ -1,3 +1,14 @@
+resource "grafana_data_source" "cloudwatch" {
+  type       = "cloudwatch"
+  name       = local.name
+  is_default = true
+  json_data {
+    sigv4_auth      = true
+    sigv4_auth_type = "workspace-iam-role"
+    sigv4_region    = var.aws_region
+  }
+}
+
 resource "grafana_dashboard" "this" {
   folder      = var.dashboards_folder_id
   config_json = file("${path.module}/dashboards/amp-dashboard.json")

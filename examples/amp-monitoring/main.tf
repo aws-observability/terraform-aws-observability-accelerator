e23f386eb37071a9afb67b25003360435240aa88
Kevin Lewin <lewinke@amazon.com>
2022-10-27 11:38:36 -0400

    resolving feedback

--- examples/amp-monitoring/main.tf
+++ examples/amp-monitoring/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = local.region
+  region = local.region
 }
 
 provider "aws" {
   region = "us-east-1"
-  alias = "billing_region"
+  alias  = "billing_region"
 }
 
 provider "grafana" {
@@ -18,13 +18,13 @@ data "aws_grafana_workspace" "this" {
 }
 
 locals {
-    region = var.aws_region
-    amg_ws_endpoint = var.managed_grafana_workspace_id == "" ? "https://${module.managed_grafana[0].workspace_endpoint}" : "https://${data.aws_grafana_workspace.this[0].endpoint}"
-    amg_ws_id       = var.managed_grafana_workspace_id == "" ? split(".", module.managed_grafana[0].workspace_endpoint)[0] : var.managed_grafana_workspace_id
-    tags = {
-        Source = "github.com/aws-observability/terraform-aws-observability-accelerator"
-    }
-    name = "aws-observability-accelerator"
+  region          = var.aws_region
+  amg_ws_endpoint = var.managed_grafana_workspace_id == "" ? "https://${module.managed_grafana[0].workspace_endpoint}" : "https://${data.aws_grafana_workspace.this[0].endpoint}"
+  amg_ws_id       = var.managed_grafana_workspace_id == "" ? split(".", module.managed_grafana[0].workspace_endpoint)[0] : var.managed_grafana_workspace_id
+  tags = {
+    Source = "github.com/aws-observability/terraform-aws-observability-accelerator"
+  }
+  name = "aws-observability-accelerator"
 }
 
 module "managed_grafana" {
@@ -51,19 +51,18 @@ output "grafana_dashboards_folder_id" {
 }
 
 module "amp_monitor" {
-    source = "../../modules/workloads/amp-monitoring"
-    dashboards_folder_id = resource.grafana_folder.this.id
-    aws_region = local.region
-    managed_prometheus_workspace_id = var.managed_prometheus_workspace_id
-    depends_on = [
+  source                          = "../../modules/workloads/amp-monitoring"
+  dashboards_folder_id            = resource.grafana_folder.this.id
+  aws_region                      = local.region
+  managed_prometheus_workspace_id = var.managed_prometheus_workspace_id
+  depends_on = [
     resource.grafana_folder.this
-    ]
+  ]
 }
 
 module "billing" {
   source = "../../modules/Billing"
   providers = {
     aws = aws.billing_region
-   }
+  }
 }
-
--- modules/workloads/amp-monitoring/main.tf
+++ modules/workloads/amp-monitoring/main.tf
@@ -13,8 +13,3 @@ resource "grafana_dashboard" "this" {
   folder      = var.dashboards_folder_id
   config_json = file("${path.module}/dashboards/amp-dashboard.json")
 }
-
-
-
-
-

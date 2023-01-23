e8b86d1e77e898d795f6284cc29ae937e9c93076
charlie keegan <chakeega@amazon.com>
2023-01-23 17:20:45 -0500

    update outputs naming convention as per terraform guidelines

--- examples/managed-grafana-setup/outputs.tf
+++ examples/managed-grafana-setup/outputs.tf
@@ -1,9 +1,9 @@
-output "grafana-workspace-endpoint" {
+output "grafana_workspace_endpoint" {
   description = "The Grafana Workspace endpoint"
   value       = aws_grafana_workspace.workshop.endpoint
 }
 
-output "grafana-workspace-id" {
+output "grafana_workspace_id" {
   description = "The Grafana Workspace ID"
   value       = aws_grafana_workspace.workshop.id
 }

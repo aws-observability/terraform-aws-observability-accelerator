1eed3ee04168c4592221008c55597d865b8210f5
Prithvi Reddy <ypreddy@amazon.com>
2023-01-27 14:33:58 -0600

    new dashboards.tf

--- modules/workloads/istio/dashboards.tf
+++ modules/workloads/istio/dashboards.tf
@@ -1,36 +1,36 @@
-resource "grafana_dashboard" "workloads" {
-  count       = var.enable_dashboards ? 1 : 0
-  folder      = var.dashboards_folder_id
-  config_json = file("${path.module}/dashboards/workloads.json")
-}
-
-resource "grafana_dashboard" "nodes" {
-  count       = var.enable_dashboards ? 1 : 0
-  folder      = var.dashboards_folder_id
-  config_json = file("${path.module}/dashboards/nodes.json")
-}
-
-resource "grafana_dashboard" "nsworkload" {
-  count       = var.enable_dashboards ? 1 : 0
-  folder      = var.dashboards_folder_id
-  config_json = file("${path.module}/dashboards/namespace-workloads.json")
-}
-
-
-resource "grafana_dashboard" "kubelet" {
-  count       = var.enable_dashboards ? 1 : 0
-  folder      = var.dashboards_folder_id
-  config_json = file("${path.module}/dashboards/kubelet.json")
-}
-
-resource "grafana_dashboard" "cluster" {
+resource "grafana_dashboard" “cluster” {
   count       = var.enable_dashboards ? 1 : 0
   folder      = var.dashboards_folder_id
   config_json = file("${path.module}/dashboards/cluster.json")
 }
 
-resource "grafana_dashboard" "nodeexp_nodes" {
+resource "grafana_dashboard" "istio-mesh-dashboard" {
   count       = var.enable_dashboards ? 1 : 0
   folder      = var.dashboards_folder_id
-  config_json = file("${path.module}/dashboards/nodeexporter-nodes.json")
+  config_json = file("${path.module}/dashboards/istio-mesh-dashboard.json")
+}
+
+resource "grafana_dashboard" "istio-service-dashboard" {
+  count       = var.enable_dashboards ? 1 : 0
+  folder      = var.dashboards_folder_id
+  config_json = file("${path.module}/dashboards/istio-service-dashboard.json")
+}
+
+
+resource "grafana_dashboard" "istio-control-plane-dashboard." {
+  count       = var.enable_dashboards ? 1 : 0
+  folder      = var.dashboards_folder_id
+  config_json = file("${path.module}/dashboards/istio-control-plane-dashboard.json)
+}
+
+resource "grafana_dashboard" “istio-performance-dashboard" {
+  count       = var.enable_dashboards ? 1 : 0
+  folder      = var.dashboards_folder_id
+  config_json = file("${path.module}/dashboards/istio-performance-dashboard.json")
+}
+
+resource "grafana_dashboard" "workloads" {
+  count       = var.enable_dashboards ? 1 : 0
+  folder      = var.dashboards_folder_id
+  config_json = file("${path.module}/dashboards/workloads.json")
 }

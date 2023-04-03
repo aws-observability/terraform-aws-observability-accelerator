9794d93d594072ad1386b253d8c626c9c6cef365
Prithvi Reddy <47993564+preddy727@users.noreply.github.com>
2023-04-03 16:35:11 -0500

    Update variables.tf

--- examples/existing-cluster-istio-monitoring/variables.tf
+++ examples/existing-cluster-istio-monitoring/variables.tf
@@ -1,26 +1,26 @@
 variable "eks_cluster_id" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "eks-cluster-with-vpc"
+#  default     = "eks-cluster-with-vpc"
 }
 variable "aws_region" {
   description = "AWS Region"
   type        = string
-  default     = "us-east-1" 
+#  default     = "us-east-1" 
 }
 variable "managed_prometheus_workspace_id" {
   description = "Amazon Managed Service for Prometheus Workspace ID"
   type        = string
-  default     = "ws-0337f531-5732-4361-acfe-e1800abc785f"
+#  default     = ""
 }
 variable "managed_grafana_workspace_id" {
   description = "Amazon Managed Grafana Workspace ID"
   type        = string
-  default     = "g-120c5f0253"
+#  default     = ""
 }
 variable "grafana_api_key" {
   description = "API key for authorizing the Grafana provider to make changes to Amazon Managed Grafana"
   type        = string
-  default     = "eyJrIjoic2tWTm5Nb0JSZnNTalMxOWtENndwMzNTekw3b09vMWUiLCJuIjoiRGVtbyIsImlkIjoxfQ=="
+#  default     = ""
   sensitive   = true
 }

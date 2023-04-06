54b9faa22f395cbb1b1552b438cc5d5f6c643085
Prithvi Reddy <ypreddy@amazon.com>
2023-04-06 14:02:10 -0500

    dasbhoards

--- modules/eks-monitoring/patterns/istio/main.tf
+++ modules/eks-monitoring/patterns/istio/main.tf
@@ -30,12 +30,13 @@ groups:
 EOF
 }
 
+/*
 resource "grafana_dashboard" "istiomeshdashboard" {
   count       = var.enable_dashboards ? 1 : 0
   folder      = var.dashboards_folder_id
   config_json = file("${path.module}/dashboards/istio-mesh-dashboard.json")
 }
-
+*/
 resource "grafana_dashboard" "istioservicedashboard" {
   count       = var.enable_dashboards ? 1 : 0
   folder      = var.dashboards_folder_id

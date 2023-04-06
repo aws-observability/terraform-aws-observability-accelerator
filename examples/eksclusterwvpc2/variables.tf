5f83e91a7bb48bf5b32d614b356ac7683a6903ab
Prithvi Reddy <47993564+preddy727@users.noreply.github.com>
2023-04-06 10:04:49 -0500

    Delete examples/eksclusterwvpc2 directory

--- examples/eksclusterwvpc2/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cluster_name" {
-  description = "Name of cluster - used by Terratest for e2e test automation"
-  type        = string
-  default     = ""
-}
-variable "aws_region" {
-  description = "AWS Region"
-  type        = string
-}

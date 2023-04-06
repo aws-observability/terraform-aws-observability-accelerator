1c8f48d1903e426a48e4f77d8249c033d278c5c6
Prithvi Reddy <ypreddy@amazon.com>
2023-04-06 13:59:43 -0500

    dasbhoard

--- modules/eks-monitoring/patterns/istio/outputs.tf
+++ modules/eks-monitoring/patterns/istio/outputs.tf
@@ -1,7 +1,6 @@
 output "grafana_dashboard_urls" {
   value = [concat(
     grafana_dashboard.istiocontrolplanedashboard[*].url,
-    grafana_dashboard.istiomeshdashboard[*].url,
     grafana_dashboard.istioperformancedashboard[*].url,
     grafana_dashboard.istioservicedashboard[*].url
   )]

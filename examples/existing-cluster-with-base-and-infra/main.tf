bb1f056f8feeda5bac759f4e2f84c6f29809ac8f
Elamaran Shanmugam <elamaran.shan@gmail.com>
2023-04-11 20:53:14 +0000

    Fixing Documentation

--- examples/existing-cluster-with-base-and-infra/main.tf
+++ examples/existing-cluster-with-base-and-infra/main.tf
@@ -81,7 +81,7 @@ module "eks_monitoring" {
   enable_external_secrets = true
   grafana_api_key         = var.grafana_api_key
   target_secret_name      = "grafana-admin-credentials"
-  target_secret_namespace      = "grafana-operator"
+  target_secret_namespace = "grafana-operator"
 
   # control the publishing of dashboards by specifying the boolean value for the variable 'enable_dashboards', default is 'true'
   # the intention to publish is overruled depending upon whether grafana dashboard folder is created by the observability accelerator

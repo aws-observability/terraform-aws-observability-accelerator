8d44533f293816048fbc606575d4ca00bf8144c0
Elamaran Shanmugam <elamaran.shan@gmail.com>
2023-06-03 09:16:45 -0400

    Fixing Grafana Operator Version and cleaning full boards

--- modules/eks-monitoring/main.tf
+++ modules/eks-monitoring/main.tf
@@ -61,51 +61,6 @@ resource "helm_release" "fluxcd" {
   }
 }
 
-resource "kubectl_manifest" "flux_gitrepository" {
-  yaml_body  = <<YAML
-apiVersion: source.toolkit.fluxcd.io/v1beta2
-kind: GitRepository
-metadata:
-  name: grafana-repo
-  namespace: flux-system
-spec:
-  interval: 5m0s
-  url: https://github.com/aws-observability/aws-observability-accelerator
-  ref:
-    branch: main
-YAML
-  depends_on = [module.external_secrets]
-}
-
-resource "kubectl_manifest" "flux_kustomization" {
-  yaml_body  = <<YAML
-apiVersion: kustomize.toolkit.fluxcd.io/v1beta2
-kind: Kustomization
-metadata:
-  name: grafana-kustomization
-  namespace: flux-system
-spec:
-  interval: 1m0s
-  path: ./artifacts/grafana-operator-manifests
-  prune: true
-  sourceRef:
-    kind: GitRepository
-    name: grafana-repo
-  postBuild:
-    substitute:
-      AMG_AWS_REGION: ${var.managed_prometheus_workspace_region}
-      AMP_ENDPOINT_URL: ${var.managed_prometheus_workspace_endpoint}
-      AMG_ENDPOINT_URL: ${var.grafana_url}
-      GRAFANA_CLUSTER_DASH_URL : ${var.grafana_cluster_dashboard_url},
-      GRAFANA_KUBELET_DASH_URL : ${var.grafana_kubelet_dashboard_url},
-      GRAFANA_NSWRKLDS_DASH_URL : ${var.grafana_namespace_workloads_dashboard_url},
-      GRAFANA_NODEEXP_DASH_URL ${var.grafana_node_exporter_dashboard_url}
-      GRAFANA_NODES_DASH_URL: ${var.grafana_nodes_dashboard_url},
-      GRAFANA_WORKLOADS_DASH_URL: ${var.grafana_workloads_dashboard_url}
-YAML
-  depends_on = [module.external_secrets]
-}
-
 resource "helm_release" "grafana_operator" {
   count            = var.enable_grafana_operator ? 1 : 0
   chart            = var.go_config.helm_chart

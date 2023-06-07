3d8002e9cba58e5c9f9667f0802e867f6e7aa0eb
Rodrigue Koffi <bonclay7@users.noreply.github.com>
2023-06-07 23:13:31 +0200

    Drop dead variable

--- locals.tf
+++ locals.tf
@@ -12,7 +12,6 @@ locals {
   amp_ws_endpoint = "https://aps-workspaces.${local.amp_ws_region}.amazonaws.com/workspaces/${local.amp_ws_id}/"
 
   amg_ws_endpoint = "https://${data.aws_grafana_workspace.this.endpoint}"
-  amg_ws_id       = var.managed_grafana_workspace_id
 
   name = "aws-observability-accelerator"
 }

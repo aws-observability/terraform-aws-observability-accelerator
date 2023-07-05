aac831b685e3a1c5879144e9fd02c55cb70011b3
Prithvi Reddy <ypreddy@amazon.com>
2023-07-05 14:20:44 -0500

    Test

--- modules/eks-monitoring/patterns/istio/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "grafana_dashboard_urls" {
-  value = [concat(
-    grafana_dashboard.istiocontrolplanedashboard[*].url,
-    grafana_dashboard.istioperformancedashboard[*].url,
-    grafana_dashboard.istioservicedashboard[*].url
-  )]
-  description = "URLs for dashboards created"
-}

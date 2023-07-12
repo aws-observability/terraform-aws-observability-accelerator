d3ce2bf5c7cf72f750874358e62d55891a6dbf1e
Prithvi Reddy <47993564+preddy727@users.noreply.github.com>
2023-07-12 15:45:43 -0500

    Update outputs.tf

--- examples/eks-istio/outputs.tf
+++ examples/eks-istio/outputs.tf
@@ -3,6 +3,16 @@ output "aws_region" {
   value       = module.aws_observability_accelerator.aws_region
 }
 
+output "managed_prometheus_workspace_endpoint" {
+  description = "Amazon Managed Prometheus workspace endpoint"
+  value       = module.aws_observability_accelerator.managed_prometheus_workspace_endpoint
+}
+
+output "managed_prometheus_workspace_id" {
+  description = "Amazon Managed Prometheus workspace ID"
+  value       = module.aws_observability_accelerator.managed_prometheus_workspace_id
+}
+
 output "eks_cluster_version" {
   description = "EKS Cluster version"
   value       = module.eks_monitoring.eks_cluster_version

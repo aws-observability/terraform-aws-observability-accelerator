97177eea04116e6994ac05a9c2bf9a827c39afc2
Rajat Omar <rajatomar550@gmail.com>
2023-08-09 21:53:54 +0530

    EKS container insights (#204)
    * adot-container-insight-tf-code by Rajat Omar
    
    * Documentation and naming convention added
    
    * PR review fixes
    
    * PR CI pipeline fixes
    
    * pr ci run fixes
    
    * added the variables mentioned in ci build
    
    * ci variable fixes
    
    * changed the module name
    
    ---------
    
    Co-authored-by: Omar <merajat@3c0630162a5a.ant.amazon.com>

---
+++ examples/eks-container-insights/versions.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
+    }
+  }
+
+  # ##  Used for end-to-end testing on project; update to suit your needs
+  # backend "s3" {
+  #   bucket = "aws-observability-accelerator-terraform-states"
+  #   region = "us-west-2"
+  #   key    = "e2e/eks_container_insights/terraform.tfstate"
+  # }
+
+}
---
+++ modules/eks-container-insights/versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
+    }
+  }
+}

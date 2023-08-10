e2e7af7719093ad6fd7bf3bdf220318594e5b4eb
Ruchika Modi <ruchimo@amazon.com>
2023-08-10 11:41:55 +0000

    Restructuring Examples and modules folder for ECS, Added content in main Readme

--- examples/ecs-cluster-with-vpc/main.tf
+++ examples/ecs-cluster-with-vpc/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  region = "eu-west-1"
+  region = "us-east-1"
   name   = "ex-${basename(path.cwd)}"
 
   vpc_cidr = "10.0.0.0/16"
@@ -26,12 +26,15 @@ locals {
 ################################################################################
 
 module "ecs_cluster" {
-  source = "./modules/cluster"
+  source = "terraform-aws-modules/ecs/aws"
 
   cluster_name = local.name
 
   # Capacity provider - autoscaling groups
   default_capacity_provider_use_fargate = false
+  create_task_exec_iam_role = true
+  task_exec_iam_role_name = "ecs_monitor_task_exec_role"
+  task_exec_iam_role_policies = {"module.ecs_cluster.module.cluster.aws_iam_policy.task_exec[0]" : "arn:aws:iam::aws:policy/AmazonPrometheusRemoteWriteAccess" }
   autoscaling_capacity_providers = {
     # On-demand instances
     ex-1 = {
@@ -50,164 +53,11 @@ module "ecs_cluster" {
         base   = 20
       }
     }
-    # Spot instances
-    ex-2 = {
-      auto_scaling_group_arn         = module.autoscaling["ex-2"].autoscaling_group_arn
-      managed_termination_protection = "ENABLED"
-
-      managed_scaling = {
-        maximum_scaling_step_size = 15
-        minimum_scaling_step_size = 5
-        status                    = "ENABLED"
-        target_capacity           = 90
-      }
-
-      default_capacity_provider_strategy = {
-        weight = 40
-      }
-
-      cluster_settings = {
-        name  = "containerInsights"
-        value = "disabled"
-      }
-    }
   }
 
   tags = local.tags
 }
 
-################################################################################
-# Service
-################################################################################
-
-module "ecs_service" {
-  source = "./modules/service"
-
-  # Service
-  name        = local.name
-  cluster_arn = module.ecs_cluster.arn
-
-  # Task Definition
-  requires_compatibilities = ["EC2"]
-  capacity_provider_strategy = {
-    # On-demand instances
-    ex-1 = {
-      capacity_provider = module.ecs_cluster.autoscaling_capacity_providers["ex-1"].name
-      weight            = 1
-      base              = 1
-    }
-  }
-
-  volume = {
-    my-vol = {}
-  }
-
-  # Container definition(s)
-  container_definitions = {
-    (local.container_name) = {
-      image = "public.ecr.aws/ecs-sample-image/amazon-ecs-sample:latest"
-      port_mappings = [
-        {
-          name          = local.container_name
-          containerPort = local.container_port
-          protocol      = "tcp"
-        }
-      ]
-
-      mount_points = [
-        {
-          sourceVolume  = "my-vol",
-          containerPath = "/var/www/my-vol"
-        }
-      ]
-
-      entry_point = ["/usr/sbin/apache2", "-D", "FOREGROUND"]
-
-      # Example image used requires access to write to root filesystem
-      readonly_root_filesystem = false
-    }
-  }
-
-  load_balancer = {
-    service = {
-      target_group_arn = element(module.alb.target_group_arns, 0)
-      container_name   = local.container_name
-      container_port   = local.container_port
-    }
-  }
-
-  subnet_ids = module.vpc.private_subnets
-  security_group_rules = {
-    alb_http_ingress = {
-      type                     = "ingress"
-      from_port                = local.container_port
-      to_port                  = local.container_port
-      protocol                 = "tcp"
-      description              = "Service port"
-      source_security_group_id = module.alb_sg.security_group_id
-    }
-  }
-
-  tags = local.tags
-}
-
-################################################################################
-# Supporting Resources
-################################################################################
-
-# https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html#ecs-optimized-ami-linux
-data "aws_ssm_parameter" "ecs_optimized_ami" {
-  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
-}
-
-module "alb_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 5.0"
-
-  name        = "${local.name}-service"
-  description = "Service security group"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_rules       = ["http-80-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = module.vpc.private_subnets_cidr_blocks
-
-  tags = local.tags
-}
-
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
-
-  name = local.name
-
-  load_balancer_type = "application"
-
-  vpc_id          = module.vpc.vpc_id
-  subnets         = module.vpc.public_subnets
-  security_groups = [module.alb_sg.security_group_id]
-
-  http_tcp_listeners = [
-    {
-      port               = local.container_port
-      protocol           = "HTTP"
-      target_group_index = 0
-    },
-  ]
-
-  target_groups = [
-    {
-      name             = "${local.name}-${local.container_name}"
-      backend_protocol = "HTTP"
-      backend_port     = local.container_port
-      target_type      = "ip"
-    },
-  ]
-
-  tags = local.tags
-}
 
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
@@ -226,39 +76,6 @@ module "autoscaling" {
         ECS_LOGLEVEL=debug
         ECS_CONTAINER_INSTANCE_TAGS=${jsonencode(local.tags)}
         ECS_ENABLE_TASK_IAM_ROLE=true
-        EOF
-      EOT
-    }
-    # Spot instances
-    ex-2 = {
-      instance_type              = "t3.medium"
-      use_mixed_instances_policy = true
-      mixed_instances_policy = {
-        instances_distribution = {
-          on_demand_base_capacity                  = 0
-          on_demand_percentage_above_base_capacity = 0
-          spot_allocation_strategy                 = "price-capacity-optimized"
-        }
-
-        override = [
-          {
-            instance_type     = "m4.large"
-            weighted_capacity = "2"
-          },
-          {
-            instance_type     = "t3.large"
-            weighted_capacity = "1"
-          },
-        ]
-      }
-      user_data = <<-EOT
-        #!/bin/bash
-        cat <<'EOF' >> /etc/ecs/ecs.config
-        ECS_CLUSTER=${local.name}
-        ECS_LOGLEVEL=debug
-        ECS_CONTAINER_INSTANCE_TAGS=${jsonencode(local.tags)}
-        ECS_ENABLE_TASK_IAM_ROLE=true
-        ECS_ENABLE_SPOT_INSTANCE_DRAINING=true
         EOF
       EOT
     }
@@ -340,11 +157,32 @@ module "vpc" {
   tags = local.tags
 }
 
-module "managed_ecs_monitoring" {
+data "aws_ssm_parameter" "ecs_optimized_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
+}
+
+module "alb_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = "${local.name}-service"
+  description = "Service security group"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_rules       = ["http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = module.vpc.private_subnets_cidr_blocks
+
+  tags = local.tags
+}
+
+module "ecs_monitoring" {
   source                = "../../modules/ecs-monitoring"
-  aws_ecs_cluster_name  = var.aws_ecs_cluster_name
-  taskRoleArn           = var.taskRoleArn
-  executionRoleArn      = var.executionRoleArn
+  aws_ecs_cluster_name  = module.ecs_cluster.cluster_name
+  taskRoleArn           = module.ecs_cluster.task_exec_iam_role_arn
+  executionRoleArn      = module.ecs_cluster.task_exec_iam_role_arn
   
   depends_on = [
     module.ecs_service

bdcdc0d334f5aa10f45b81a2aa4dbf261ca559c8
Ruchika Modi <ruchimo@amazon.com>
2023-10-30 08:20:34 +0000

    Fixing failures detected by pre-commit

--- modules/ecs-monitoring/outputs.tf
+++ modules/ecs-monitoring/outputs.tf
@@ -7,13 +7,3 @@ output "grafana_workspace_endpoint" {
   description = "The endpoint of the Grafana workspace"
   value       = module.managed_grafana_default.workspace_endpoint
 }
-
-output "prometheus_workspace_id" {
-  description = "Identifier of the workspace"
-  value       = module.managed_prometheus_default.workspace_id
-}
-
-output "prometheus_workspace_endpoint" {
-  description = "Prometheus endpoint available for this workspace"
-  value       = module.managed_prometheus_default.workspace_prometheus_endpoint
-}

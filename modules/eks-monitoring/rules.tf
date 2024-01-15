28689b83e825b0d69bd477f34a96591edcd49afa
Rodrigue Koffi <bonclay7@users.noreply.github.com>
2024-01-15 16:37:34 -0500

    Fix workspace input

--- modules/eks-monitoring/rules.tf
+++ modules/eks-monitoring/rules.tf
@@ -7,7 +7,7 @@
 resource "aws_prometheus_rule_group_namespace" "recording_rules" {
   count        = var.enable_recording_rules ? 1 : 0
   name         = "accelerator-infra-rules"
-  workspace_id = var.managed_prometheus_workspace_id
+  workspace_id = local.managed_prometheus_workspace_id
   data         = <<EOF
 groups:
   - name: infra-rules-01

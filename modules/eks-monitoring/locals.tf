ada16d59655d3640abc699d77ba8a8131d6f0a43
Kevin Lewin <97046295+lewinkedrs@users.noreply.github.com>
2024-01-24 12:09:52 -0500

    feat(module: eks-monitoring) Add NVIDIA gpu monitoring dashboards (#257)
    * gpu dashboards
    
    * fixing locals
    
    * doc start
    
    * Update gpumon.md
    
    * fixing typos and doc names
    
    * fixing module name
    
    * fixing mkdocs
    
    * gpu to nvidia
    
    * Apply pre-commit
    
    ---------
    
    Co-authored-by: Rodrigue Koffi <bonclay7@users.noreply.github.com>

--- modules/eks-monitoring/locals.tf
+++ modules/eks-monitoring/locals.tf
@@ -132,6 +132,15 @@ locals {
     }
   }
 
+  nvidia_monitoring_config = {
+    # can be overriden by providing a config
+    flux_gitrepository_name   = try(var.nvidia_monitoring_config.flux_gitrepository_name, var.flux_gitrepository_name)
+    flux_gitrepository_url    = try(var.nvidia_monitoring_config.flux_gitrepository_url, var.flux_gitrepository_url)
+    flux_gitrepository_branch = try(var.nvidia_monitoring_config.flux_gitrepository_branch, var.flux_gitrepository_branch)
+    flux_kustomization_name   = try(var.nvidia_monitoring_config.flux_kustomization_name, "grafana-dashboards-adothealth")
+    flux_kustomization_path   = try(var.nvidia_monitoring_config.flux_kustomization_path, "./artifacts/grafana-operator-manifests/eks/gpu")
+  }
+
   kubeproxy_monitoring_config = {
     # can be overriden by providing a config
     flux_gitrepository_name   = try(var.kubeproxy_monitoring_config.flux_gitrepository_name, var.flux_gitrepository_name)
